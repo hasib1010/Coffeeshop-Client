cf047eba1fd0ecebb42ea615f4a431a8207c0844
hasib1010 <asifmahmud664@gmail.com>
2024-01-29 19:00:05 +0600

    update added

--- src/CoffeeLayout.jsx
+++ src/CoffeeLayout.jsx
@@ -1,24 +1,66 @@
 import React from 'react';
-import { FaEye, FaPen  } from "react-icons/fa";
+import { FaEye, FaPen } from "react-icons/fa";
 import { AiFillDelete } from "react-icons/ai";
+import Swal from 'sweetalert2'
+import { Link } from 'react-router-dom';
+
 const CoffeeLayout = ({ coffee }) => {
-    console.log(coffee);
-    const { chef, details, name, photo, supplier, taste } = coffee;
-    return (
-        <div className='flex items-center  gap-11 border-2 w-fit p-4 rounded-lg drop-shadow-lg'>
-          <div className='flex flex-col items-center justify-center'>
-          <img src={photo} alt="" />
-            <h1>{name}</h1>
-            <p>{chef}</p>
-            <p>{details}</p>
-          </div>
-          <div>
-            <FaPen className='text-2xl my-2 cursor-pointer hover:text-red-700 '></FaPen>
-            <FaEye className='text-2xl my-2 cursor-pointer hover:text-red-700 '></FaEye>
-            <AiFillDelete className='text-2xl my-2 cursor-pointer hover:text-red-700 '></AiFillDelete>
-          </div>
+  console.log(coffee);
+  const { chef, details, name, photo, supplier, taste, _id } = coffee;
+  const handleDelete = (_id) => {
+    // console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then(res => res.json())
+          .then(data => {
+            // console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your Coffee has been deleted.",
+                icon: "success"
+              });
+            }
+          })
+      }
+
+
+    });
+  }
+  return (
+    <div className='flex items-center  gap-11 border-2 w-fit p-4 rounded-lg drop-shadow-lg'>
+      <div className='flex flex-col items-center justify-center'>
+        <img src={photo} alt="" />
+        <h1>{name}</h1>
+        <p>{chef}</p>
+        <p>{details}</p>
+      </div>
+      <div className='flex flex-col gap-5'>
+        <div className='btn w-fit px-2  hover:text-red-700 '>
+          <FaEye className='text-2xl my-2 cursor-pointer '></FaEye>
         </div>
-    );
+        <Link to={`/updateCoffee/${_id}`}>
+        <div className='btn w-fit px-2  hover:text-red-700 '>
+          <FaPen className='text-2xl my-2 cursor-pointer '></FaPen>
+        </div></Link>
+        <div onClick={() => handleDelete(_id)} className='  hover:text-red-700 btn w-fit px-2'>
+          <AiFillDelete className='text-2xl my-2 cursor-pointer '></AiFillDelete>
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default CoffeeLayout;
--- src/Component/updateCoffee.jsx
+++ src/Component/updateCoffee.jsx
@@ -1,9 +1,90 @@
 import React from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const UpdateCoffee = () => {
+    const coffee = useLoaderData();
+    const { chef, details, name, photo, supplier, taste, _id, category } = coffee;
+    const updateCoffee = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.defaultValue;
+        const chef = form.chef.defaultValue;
+        const supplier = form.supplier.defaultValue;
+        const taste = form.taste.defaultValue;
+        const category = form.category.defaultValue;
+        const details = form.details.defaultValue;
+        const photo = form.photo.defaultValue;
+        const updatedCoffee = { name, chef, supplier, taste, category, details, photo };
+        console.log(updatedCoffee);
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(updatedCoffee)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount>0) {
+                    Swal.fire({
+                        title: 'Congratulations!',
+                        text: 'Coffee Added successfully!',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    });
+                } 
+            })
+        // reset()
+    }
     return (
         <div>
-            <h1>Update Coffee</h1>
+           <Link to={'/'}>Home</Link>
+            <div className='container mx-auto '>
+                <form onSubmit={updateCoffee}   className='w-4/5 mx-auto mt-4 bg-[#F4F3F0] min-h-svh'>
+                    <h1 className='text-center  font-bold text-5xl py-6'> <h1>Update details of {name} </h1></h1>
+                     
+                    <div className='flex justify-around'>
+
+                        <label className='mt-5'>
+                            <h2 className='text-xl '>Name</h2>
+                            <input type="text" name='name' defaultValue={name} placeholder='Enter coffee name' className='p-3 placeholder:text-red-600 border rounded-xl mt-3    w-96  border-black' />
+                        </label>
+                        <label className='mt-5'>
+                            <h2 className='text-xl '>Chef</h2>
+                            <input type="text" name='chef' defaultValue={chef} placeholder='Enter coffee chef' className='p-3 placeholder:text-red-600 border  rounded-xl mt-3   border-black w-96' />
+                        </label>
+                    </div>
+                    <div className='flex justify-around'>
+                        <label className='mt-5'>
+                            <h2 className='text-xl '>Supplier</h2>
+                            <input type="text" name='supplier'  defaultValue={supplier} placeholder='Enter coffee supplier' className='p-3 placeholder:text-red-600 border rounded-xl mt-3    w-96  border-black' />
+                        </label>
+                        <label className='mt-5'>
+                            <h2 className='text-xl '>Taste</h2>
+                            <input type="text" name='taste'  defaultValue={taste} placeholder='Enter coffee taste' className='p-3 placeholder:text-red-600 border  rounded-xl mt-3   border-black w-96' />
+                        </label>
+                    </div>
+                    <div className='flex justify-around'>
+                        <label className='mt-5'>
+                            <h2 className='text-xl '>Category</h2>
+                            <input type="text" name='category'  defaultValue={category} placeholder='Enter coffee Category' className='p-3 placeholder:text-red-600 border rounded-xl mt-3    w-96  border-black' />
+                        </label>
+                        <label className='mt-5'>
+                            <h2 className='text-xl '>Details</h2>
+                            <input type="text" name='details'  defaultValue={details} placeholder='Enter coffee Details' className='p-3 placeholder:text-red-600 border  rounded-xl mt-3   border-black w-96' />
+                        </label>
+                    </div>
+                    <div className='w-4/5 mx-auto mt-7'>
+                        <h2 className="text-xl">Photo</h2>
+                        <input name='photo' defaultValue={photo} className='   w-3/4  rounded-md border h-12 pl-4' placeholder='Enter photo URL' type="text" />
+                    </div>
+
+                    <div className='flex items-center  justify-center'>
+                        <input className='w-5/6   mt-7 btn btn-warning' type="submit" defaultValue="Update Coffee" />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
